1ba6b1b009ef06a4408aeb684d8f9de6f77d7359
sumonpaul55 <sumpaul100@gmail.com>
2024-05-18 03:18:20 +0600

    global error handler

--- dist/app/app.js
+++ dist/app/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -13,6 +22,11 @@ const userRouter = express_1.default.Router();
 const coureRouter = express_1.default.Router();
 app.use("/api/v1/course", coureRouter);
 app.use("/api/v1/users", userRouter);
+// middleware
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
 userRouter.post("/create-user", (req, res) => {
     const user = req.body;
     console.log(user);
@@ -22,14 +36,17 @@ coureRouter.post("/create-course", (req, res) => {
     console.log("courser", req.body);
     res.json({ message: "success" });
 });
-// middleware
-const logger = (req, res, next) => {
-    console.log(req.url, req.method, req.hostname);
-    next();
-};
-app.get('/', logger, (req, res) => {
-    res.send('Hello paul! world again paul');
-});
+app.get('/', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        //   console.log(req.params)
+        res.send(something);
+    }
+    catch (error) {
+        next(error);
+        // console.log(error)
+        // res.status(400).json({success:false, message: "something went wrong"})
+    }
+}));
 app.post("/sumon/:id/:page", logger, (req, res) => {
     console.log(req.params);
     res.json({ message: "Success" });
@@ -37,5 +54,12 @@ app.post("/sumon/:id/:page", logger, (req, res) => {
 app.post("/query", logger, (req, res) => {
     console.log(req.query);
     res.json({ message: "Success" });
+});
+// global error handler
+app.use((error, req, res, next) => {
+    console.log(error, req, res, next);
+    if (error) {
+        res.status(400).json({ success: false, message: "something error" });
+    }
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, { NextFunction, Request, response, Response } from 'express';
 const app = express()
 // parser
 app.use(express.json())
@@ -10,6 +10,22 @@ const coureRouter = express.Router()
 
 app.use("/api/v1/course", coureRouter)
 app.use("/api/v1/users", userRouter)
+
+
+
+
+
+
+
+
+// middleware
+const logger = (req:Request, res:Response, next:NextFunction)=>{
+    console.log(req.url, req.method, req.hostname)
+    next()
+}
+
+
+
 
 
 userRouter.post("/create-user", (req:Request, res:Response)=>{
@@ -28,23 +44,38 @@ coureRouter.post("/create-course", (req:Request, res:Response)=>{
 
 
 
-// middleware
-const logger = (req:Request, res:Response, next:NextFunction)=>{
-    console.log(req.url, req.method, req.hostname)
-    next()
-}
+app.get('/', async(req: Request, res:Response, next:NextFunction) => {
+    try {
+    //   console.log(req.params)
+    res.send(something)
 
-app.get('/',logger, (req: Request, res:Response) => {
-  res.send('Hello paul! world again paul')
- 
+    } catch (error) {
+        next(error)
+        // console.log(error)
+        // res.status(400).json({success:false, message: "something went wrong"})
+    }
+    
 })
 
-app.post("/sumon/:id/:page",logger, (req: Request, res:Response)=>{
+app.post("/sumon/:id/:page",logger,  (req: Request, res:Response)=>{
     console.log(req.params)
     res.json({message: "Success"})
 })
 app.post("/query",logger, (req: Request, res:Response)=>{
     console.log(req.query)
     res.json({message: "Success"})
+})
+
+
+
+
+
+// global error handler
+app.use((error:any, req:Request, res:Response, next:NextFunction)=>{
+    console.log(error, req, res, next)
+    if(error){
+        res.status(400).json({success:false, message:"something error"})
+    }
+   
 })
 export default app;
